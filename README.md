cc065b4fcf1b04e1e42dabbc7c926181defbbe09
YawaB <mawuviwobrinda@gmail.com>
2023-12-19 12:18:11 +0100

    Eureka discovery service configurations

--- README.md
+++ README.md
@@ -15,7 +15,21 @@
 <hr>
 <img src="captures/configGatewayBean.png" alt="Gateway configuration with a bean function">
 <p>Gateway application.yml configuration</p>
+<hr>
 <p>Gateway configuration with a bean function</p>
 <img src="captures/testProduct.png" alt="Gateway test with the /products paths">
 <hr>
+<h3>Discovery Service started</h3>
+<img src="captures/discoveryService.png" alt="all services registering on discovery">
+<p>All services registering on Eureka discovery service</p>
+<hr>
+<p>After enabling the discovery service, we can make a dynamic configuration for the gateway</p>
+<h3>Dynamic configuration of the Gateway</h3>
+<img src="captures/dynamicConfigGateway.png" alt="dynamic config using the load balancer">
+<p>Dynamic config using the load balancer and it's still working properly</p>
+<p>We can also make the routing dynamically by using a Bean function which return a DiscoveryClientRouteDefinitionLocator and get the microservice from the request</p>
+<img src="captures/dynamicConfigUsingBean.png" alt="dynamic config using the Bean">
+<hr>
+p>Our gateway still working</p>
+<img src="captures/gatewayWorking.png" alt="dynamic config using the Bean">
 
--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -2,8 +2,9 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
@@ -13,11 +14,17 @@ public class GatewayApplication {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+//    @Bean
+//    public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
+//        return routeLocatorBuilder.routes()
+//                .route(r -> r.path("/customers/**").uri("lb://customer-service"))
+//                .route(r -> r.path("/products/**").uri("lb://inventory-service"))
+//                .build();
+//    }
+
     @Bean
-    public RouteLocator routeLocator(RouteLocatorBuilder routeLocatorBuilder) {
-        return routeLocatorBuilder.routes()
-                .route(r -> r.path("/customers/**").uri("http://localhost:8081/"))
-                .route(r -> r.path("/products/**").uri("http://localhost:8082/"))
-                .build();
+    public DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient rdc, DiscoveryLocatorProperties dlp) {
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
     }
+
 }
